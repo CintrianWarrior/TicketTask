4670a3d323e08b2554a7a01e2e991a69c1c57e95
CintrianWarrior <velegoj2@yandex.ru>
2022-11-03 18:25:31 +0300

    Ticket task complete

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 public class Main {
+    public static void stringSplit(String text) {
+        int halfText = text.length()/2;
+        String[] half = {text.substring(0, halfText), text.substring(halfText)};
+        if (text.length() % 2 != 0) {
+            System.out.println("Can not be split in half");
+        } else {
+            System.out.println(half[0] + " " + half[1]);
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("Задача из билета номер 3. Усов Никита");
         //Напишите программу, которая разделяет любую строку на две части ровно пополам по количеству символов.
-
+        String text = "афвафывфыв2";
+        int halfText = text.length()/2;
+        String[] half = {text.substring(0, halfText), text.substring(halfText)};
+        if (text.length() % 2 != 0) {
+            System.out.println("Can not be split in half");
+        } else {
+            System.out.println(half[0] + " " + half[1]);
+        }
+        stringSplit(text);
     }
 }
